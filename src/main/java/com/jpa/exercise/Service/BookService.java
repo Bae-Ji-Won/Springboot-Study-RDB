1eea6e99896d9714cb6164fadc494aef2779af08
Bae Ji Won <qowhxk@naver.com>
2022-11-26 23:06:08 +0900

    [11/23] Book Service - JPA Join기능을 통해 따로 다른 DB에서 값 안가져와도 됨(메인 Entity에서 모두 처리 가능하여 메인 Entity만 호출하여 사용)

--- src/main/java/com/jpa/exercise/Service/BookService.java
+++ src/main/java/com/jpa/exercise/Service/BookService.java
@@ -32,14 +32,7 @@ public class BookService {
     public List<BookResponse> jsonList(Pageable pageable){
         Page<Book> books = bookRepository.findAll(pageable);
         List<BookResponse> bookResponses = books.stream()
-                .map(book ->{
-                    // 위에 찾은 전체 데이터인 books에는 id,bookname,authorid 3개의 데이터로 모든 데이터가 리스트로 저장이 되 있다.
-                    // 그 중에서 authorid인 부분의 값을 가져와 authorRepository를 통해 author DB에서 데이터를 찾아와
-                    // BookResponse의 of메서드(Builder)를 통해 데이터를 매칭 시켜서 반환해준다.
-                    // 현재 이 방법은 DB에서 Join을 사용하지 않고 Jpa에서 데이터를 각자 찾아 Build를 통해 데이터를 삽입하는 방식이다.
-                    Optional<Author> optionalAuthor = authorRepository.findById(book.getAuthorId());
-                    return BookResponse.of(book,optionalAuthor.get().getName());
-                }).collect(Collectors.toList());
+            .map(book -> BookResponse.of(book)).collect(Collectors.toList());   // books에 있는 데이터를 book으로 리스트 1개씩 전부 받아 BookResponse.of로 보내어 준다.
         return bookResponses;
     }
 }
