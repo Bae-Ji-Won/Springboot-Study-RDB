dc72367f80288eae86770d128e0c555bc0684247
Bae Ji Won <qowhxk@naver.com>
2022-11-26 23:05:05 +0900

    [11/23] BookResponse DTO - Join하여 받아온 데이터 추가

--- src/main/java/com/jpa/exercise/Domain/DTO/BookResponse.java
+++ src/main/java/com/jpa/exercise/Domain/DTO/BookResponse.java
@@ -6,20 +6,22 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Builder
-public class BookResponse {
-        private long BookId;
-        private String BookName;
-        private String AuthorName;
+        @AllArgsConstructor
+        @NoArgsConstructor
+        @Getter
+        @Builder
+        public class BookResponse {
+                private long BookId;
+                private String BookName;
+                private String AuthorName;
+                private String PublisherName;
 
-        public static BookResponse of(Book book,String authorName){
-                return BookResponse.builder()
-                        .BookId(book.getId())
-                        .BookName(book.getName())
-                        .AuthorName(authorName)
-                        .build();
+                public static BookResponse of(Book book){
+                        return BookResponse.builder()
+                                .BookId(book.getId())
+                                .BookName(book.getName())
+                                .AuthorName(book.getAuthor().getName())    // Author Entity의 이름을 가져옴
+                                .PublisherName(book.getPublisher().getName())   // Publisher Entity의 이름을 가져옴
+                                .build();
+                }
         }
-}
