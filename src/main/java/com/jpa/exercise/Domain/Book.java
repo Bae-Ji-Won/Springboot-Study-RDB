fa5b56aa730323f5b4db6c3e235333ea031b483a
Bae Ji Won <qowhxk@naver.com>
2022-11-26 23:03:48 +0900

    [11/23] Book Entity - Jpa Join 추가(다른 DB 2개 연결/ FK지정)

--- src/main/java/com/jpa/exercise/Domain/Book.java
+++ src/main/java/com/jpa/exercise/Domain/Book.java
@@ -13,14 +13,16 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Book {
-    @Id
-    private Long id;
-    private String name;
-    
-     private Long authorId;
-     // => 다른 DB와 Join하여 데이터 호출
-    // JPA가 Foreign Key를 걸어준다.
-//    @ManyToOne
-//    @JoinColumn(name = "author_id")
-//    private Author author;
+        @Id
+        private Long id;
+        private String name;
+
+        // JPA가 Foreign Key를 걸어준다.
+        @ManyToOne                      // 단방향(한개 이상의 데이터를 호출할 수 있으므로, 저자가 여러명)
+        @JoinColumn(name = "author_id") // Book DB의 author_id와 아래의 변수를 Join시킴(즉, 아래의 변수를 FK로 만들어준다)
+        private Author author;      // Author 테이블을 호출함
+
+        @OneToOne                       // 단방향(무조건 한개의 데이터만 호출할 수 있으므로, 출판사는 한곳에서만)
+        @JoinColumn(name = "publisher_id")  // publisher_id를 Publisher의 FK로 연결함
+        private Publisher publisher;
 }
