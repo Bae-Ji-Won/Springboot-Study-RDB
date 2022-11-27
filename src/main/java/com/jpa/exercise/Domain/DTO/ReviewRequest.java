d9127148d58799edc368b780e8641cdbad1b1f5e
Bae Ji Won <qowhxk@naver.com>
2022-11-27 21:45:20 +0900

    [11/24] ReviewRequest DTO 생성(입력)

--- src/main/java/com/jpa/exercise/Domain/DTO/ReviewRequest.java
+++ src/main/java/com/jpa/exercise/Domain/DTO/ReviewRequest.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Builder
 public class ReviewRequest {
-    private Integer hospitalId;
+//    private Integer hospitalId;
     private String title;
     private String content;
     private String userName;
