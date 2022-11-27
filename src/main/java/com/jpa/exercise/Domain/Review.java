5846546d65eee73a5326b0bfa066bb61f055a8d7
Bae Ji Won <qowhxk@naver.com>
2022-11-27 21:44:54 +0900

    [11/24] Review Entity 생성

--- src/main/java/com/jpa/exercise/Domain/Review.java
+++ src/main/java/com/jpa/exercise/Domain/Review.java
@@ -22,8 +22,8 @@ public class Review {
     private String content;
     private String userName;
 
-    @ManyToOne
-    @JoinColumn(name = "hospital_id")
-    private Hospital hospital;
+    @ManyToOne      // 리뷰입장에서는 리뷰가 여러개고 병원은 한개이므로
+    @JoinColumn(name = "hospital_id")   // hospital_id의 행을 hospital의 pk를 가르키도록 함
+    private Hospital hospital;          // 즉, hospital_id가 FK가 됨
 
 }
