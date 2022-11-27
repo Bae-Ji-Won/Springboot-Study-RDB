9587e2bdaf2ce29aaae99c46852209bd299e1d2a
Bae Ji Won <qowhxk@naver.com>
2022-11-27 21:00:40 +0900

    [11/24] Review Entity 생성

---
+++ src/main/java/com/jpa/exercise/Domain/Review.java
@@ -0,0 +1,29 @@
+package com.jpa.exercise.Domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+    private String userName;
+
+    @ManyToOne
+    @JoinColumn(name = "hospital_id")
+    private Hospital hospital;
+
+}
