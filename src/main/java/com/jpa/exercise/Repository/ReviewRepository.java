f179b26a9e044352dd16ffea097fc4435ab66faa
Bae Ji Won <qowhxk@naver.com>
2022-11-29 23:43:53 +0900

    [11/25] ReviewRepository (FK와 연결된 테이블의 값을 호출)

--- src/main/java/com/jpa/exercise/Repository/ReviewRepository.java
+++ src/main/java/com/jpa/exercise/Repository/ReviewRepository.java
@@ -1,9 +1,14 @@
 package com.jpa.exercise.Repository;
 
+import com.jpa.exercise.Domain.DTO.ReviewReadResponse;
+import com.jpa.exercise.Domain.Hospital;
 import com.jpa.exercise.Domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review,Long> {
+    List<ReviewReadResponse> findByHospital(Hospital hospital);
 }
