a811de6d39f9adcf1fd4ebb98d5a305a0a46c3a6
Bae Ji Won <qowhxk@naver.com>
2022-11-29 23:41:18 +0900

    [11/25] Hospital Controller(1. 리뷰 등록, 2. 게시물에 해당하는 모든 리뷰 출력)

--- src/main/java/com/jpa/exercise/Controller/HospitalController.java
+++ src/main/java/com/jpa/exercise/Controller/HospitalController.java
@@ -1,10 +1,14 @@
 package com.jpa.exercise.Controller;
 
+import com.jpa.exercise.Domain.DTO.ReviewReadResponse;
 import com.jpa.exercise.Domain.DTO.ReviewRequest;
 import com.jpa.exercise.Domain.DTO.ReviewResponse;
 import com.jpa.exercise.Service.ReviewService;
+import lombok.Getter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/hospital")
@@ -15,8 +19,16 @@ public class HospitalController {
         this.reviewService = reviewService;
     }
 
+
+    // 리뷰 등록
     @PostMapping("/{id}/review")
-    public ResponseEntity<ReviewResponse> ReviewAdd(@PathVariable Integer id, @RequestBody ReviewRequest reviewRequest){
+    public ResponseEntity<ReviewResponse> ReviewAdd(@PathVariable Long id, @RequestBody ReviewRequest reviewRequest){
         return ResponseEntity.ok().body(reviewService.ReviewAdd(id,reviewRequest));
+    }
+
+    // 게시물에 대한 모든 리뷰 출력
+    @GetMapping("/{id}/reviews")
+    public ResponseEntity<List<ReviewReadResponse>> reviews(@PathVariable(name = "id") Long hospitalid){
+        return ResponseEntity.ok().body(reviewService.getTotalReview(hospitalid));
     }
 }
