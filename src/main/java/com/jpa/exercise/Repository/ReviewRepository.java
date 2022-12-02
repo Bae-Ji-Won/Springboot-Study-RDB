73125badec4b7548481f29cb427c360387f30c92
JIWON <82360230+Bae-Ji-Won@users.noreply.github.com>
2022-12-02 19:38:56 +0900

    Update ReviewRepository.java

--- src/main/java/com/jpa/exercise/Repository/ReviewRepository.java
+++ src/main/java/com/jpa/exercise/Repository/ReviewRepository.java
@@ -12,5 +12,5 @@ import java.util.List;
 public interface ReviewRepository extends JpaRepository<Review,Long> {
     List<Review> findByHospital(Hospital hospital);
     // 현재 게시물의 ID를 입력받고 그 ID를 통해 Hospital의 DB에서 데이터를 찾아 그 데이터 중 hospital_id의 값을 가지고
-    // findByHospital(hospital_id)을 통해 Review DB에서 해당 hospital_id를 가진 값들을 모두 찾아낸다.
+    // findByHospital(hospital_id)을 통해 Review DB에서 해당 hospital_id를 가진 값들을 모두  찾아낸다.
 }
