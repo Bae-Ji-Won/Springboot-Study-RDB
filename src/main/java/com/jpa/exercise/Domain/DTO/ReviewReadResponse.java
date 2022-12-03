bec72e45c1a32f94ba317ad01edfda0848916335
Bae Ji Won <qowhxk@naver.com>
2022-12-03 22:35:17 +0900

    [11/25] ReviewReadResponse DTO (Service별 Builder 코드 중복을 방지하기 위해 DTO에서 Builder기능 사용)

--- src/main/java/com/jpa/exercise/Domain/DTO/ReviewReadResponse.java
+++ src/main/java/com/jpa/exercise/Domain/DTO/ReviewReadResponse.java
@@ -1,5 +1,6 @@
 package com.jpa.exercise.Domain.DTO;
 
+import com.jpa.exercise.Domain.Review;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,4 +16,17 @@ public class ReviewReadResponse {
     private String content;
     private String patientName;
     private String hospitalName;
+
+    // Entity에서 값을 받아와 원하는 DTO 구조로 변환
+    // 이때 hospitalName은 Hospital DB에서 가져와야 하므로 관계형 DB를 통해 FK가 가르키는 DB인 Hospital에서 데이터를 가져온다.
+    public static ReviewReadResponse fromEntity(Review review) {
+        ReviewReadResponse response = ReviewReadResponse.builder()
+                .id(review.getId())
+                .title(review.getTitle())
+                .content(review.getContent())
+                .patientName(review.getUserName())
+                .hospitalName(review.getHospital().getHospitalName())
+                .build();
+        return response;
+    }
 }
