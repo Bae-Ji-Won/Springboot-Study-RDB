8c806954ac620194b1261806700f6e338977a6b7
Bae Ji Won <qowhxk@naver.com>
2022-12-03 22:33:57 +0900

    [11/25] Review Entity (Hospital Entity와 Join)

--- src/main/java/com/jpa/exercise/Domain/Review.java
+++ src/main/java/com/jpa/exercise/Domain/Review.java
@@ -16,14 +16,13 @@ import javax.persistence.*;
 public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id;            // 리뷰 id
 
-    private String title;
-    private String content;
-    private String userName;
+    private String title;       // 리뷰 제목
+    private String content;     // 리뷰 내용
+    private String userName;    // 리뷰 작성자
 
     @ManyToOne      // 리뷰입장에서는 리뷰가 여러개고 병원은 한개이므로
-    @JoinColumn(name = "hospital_id")   // hospital_id의 행을 hospital의 pk를 가르키도록 함
+    @JoinColumn(name = "hospital_id")   // hospital_id의 행을 Hospital DB와 연결함
     private Hospital hospital;          // 즉, hospital_id가 FK가 됨
-
 }
